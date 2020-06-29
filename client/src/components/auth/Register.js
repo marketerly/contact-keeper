ac3b4e3dbc2412b45f134891bc468f49e75b6dcd
marketerly <taha@marketer.ly>
2020-06-29 16:48:57 +0200

    4. User Registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,20 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
+    const authContext = useContext(AuthContext);
     const alertContext = useContext(AlertContext);
+
     const { setAlert } = alertContext;
+    const { register, error, clearErrors } = authContext;
+
+    useEffect(() => {
+        if (error === 'User already exists') {
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+    }, [error]);
 
     const [user, setUser] = useState({
         name: '',
@@ -25,6 +36,11 @@ const Register = () => {
         } else if (password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else {
+            register({
+                name,
+                email,
+                password,
+            });
             console.log('Register Submit');
         }
     };
