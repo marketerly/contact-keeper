9761d30779c79dc13a61e13c163dac1c65426762
marketerly <taha@marketer.ly>
2020-06-30 21:49:11 +0200

    6. User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
-const Login = () => {
+const Login = props => {
+    const authContext = useContext(AuthContext);
+    const alertContext = useContext(AlertContext);
+
+    const { setAlert } = alertContext;
+    const { login, error, clearErrors, isAuthenticated } = authContext;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            props.history.push('/');
+        }
+        if (error === 'Invalid Credentials') {
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+    }, [error, isAuthenticated, props.history]);
+
     const [user, setUser] = useState({
         email: '',
         password: '',
@@ -14,6 +32,14 @@ const Login = () => {
 
     const onSubmit = e => {
         e.preventDefault();
+        if (email === '' || password === '') {
+            setAlert('Please fill in all fields', 'danger');
+        } else {
+            login({
+                email,
+                password,
+            });
+        }
         console.log('Login Submit');
     };
 
--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,7 @@ router.post(
             // Check the password
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
-                return res.status(400).json({ msg: 'Invalid Credentials !' });
+                return res.status(400).json({ msg: 'Invalid Credentials' });
             }
 
             const payload = {
