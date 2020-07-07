a6170a4c73dba4bd4391a13def5b31474ca45cb6
marketerly <taha@marketer.ly>
2020-07-07 16:59:30 +0200

    3. Delete Contacts

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -5,58 +5,56 @@ import PropTypes from 'prop-types';
 const Capetalize = string => string.charAt(0).toUpperCase() + string.slice(1);
 
 const ContactItem = ({ contact }) => {
-    const contactContext = useContext(ContactContext);
-    const { deleteContact, setCurrent, clearCurrent } = contactContext;
+  const contactContext = useContext(ContactContext);
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
-    const { id, name, email, phone, type } = contact;
+  const { _id, name, email, phone, type } = contact;
 
-    const onDelete = e => {
-        deleteContact(id);
-        clearCurrent();
-    };
+  const onDelete = e => {
+    deleteContact(_id);
+    clearCurrent();
+  };
 
-    return (
-        <div className='card bg-light'>
-            <h3 className='text-primary text-left'>
-                {name}{' '}
-                <span
-                    style={{
-                        float: 'right',
-                    }}
-                    className={
-                        'badge ' +
-                        (type === 'professional'
-                            ? 'badge-success'
-                            : 'badge-primary')
-                    }>
-                    {Capetalize(type)}
-                </span>
-            </h3>
-            <ul className='list'>
-                {email && (
-                    <li>
-                        <i className='fas fa-envelope-open'></i> {email}
-                    </li>
-                )}
-                {phone && (
-                    <li>
-                        <i className='fas fa-phone'></i> {phone}
-                    </li>
-                )}
-            </ul>
-            <button
-                className='btn btn-dark btn-sm'
-                onClick={() => setCurrent(contact)}>
-                Edit
-            </button>
-            <button className='btn btn-danger btn-sm' onClick={onDelete}>
-                Delete
-            </button>
-        </div>
-    );
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span
+          style={{
+            float: 'right',
+          }}
+          className={
+            'badge ' +
+            (type === 'professional' ? 'badge-success' : 'badge-primary')
+          }>
+          {Capetalize(type)}
+        </span>
+      </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i> {phone}
+          </li>
+        )}
+      </ul>
+      <button
+        className='btn btn-dark btn-sm'
+        onClick={() => setCurrent(contact)}>
+        Edit
+      </button>
+      <button className='btn btn-danger btn-sm' onClick={onDelete}>
+        Delete
+      </button>
+    </div>
+  );
 };
 
 ContactItem.propTypes = {
-    contact: PropTypes.object.isRequired,
+  contact: PropTypes.object.isRequired,
 };
 export default ContactItem;
